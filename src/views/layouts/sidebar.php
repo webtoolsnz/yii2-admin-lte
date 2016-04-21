d4d5f20494c57c18ffec22e77f278c670a0077ff
Byron Adams <byron.adams54@gmail.com>
2016-04-21 15:49:35 +1200

    fixing Menu dependency in sidebar

--- src/views/layouts/sidebar.php
+++ src/views/layouts/sidebar.php
@@ -2,7 +2,7 @@
 
     <section class="sidebar">
         <!-- sidebar menu: : style can be found in sidebar.less -->
-        <?= \app\themes\AdminLte\widgets\Menu::widget([
+        <?= \webtoolsnz\AdminLte\widgets\Menu::widget([
             'options' => ['class' => 'sidebar-menu'],
             'items' => method_exists($this->context->module, 'getMenuItems') ?
                 $this->context->module->getMenuItems($this->context) : [],
