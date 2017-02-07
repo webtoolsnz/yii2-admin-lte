14703365393ea290ab344b8e370c283b0c8c66c0
Chris Henderson <chris@webtools.co.nz>
2017-02-08 08:37:55 +1300

    Support in header for alternative layout option

--- src/views/layouts/header.php
+++ src/views/layouts/header.php
@@ -6,20 +6,31 @@ use yii\bootstrap\Html;
 ?>
 
 <header class="main-header">
-    <!-- Logo -->
-    <a href="/" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><?= Html::img('/img/logo-alt.png', ['alt' => \Yii::$app->name]) ?></span>
-        <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><?= Html::img('/img/logo.png', ['alt' => \Yii::$app->name]) ?></span>
-    </a>
+    <?php if ($this->theme->layout == AdminLteTheme::LAYOUT_SIDEBAR_MINI) { ?>
+        <!-- Logo -->
+        <a href="/" class="logo">
+            <!-- mini logo for sidebar mini 50x50 pixels -->
+            <span class="logo-mini"><?= Html::img('/img/logo-alt.png', ['alt' => \Yii::$app->name]) ?></span>
+            <!-- logo for regular state and mobile devices -->
+            <span class="logo-lg"><?= Html::img('/img/logo.png', ['alt' => \Yii::$app->name]) ?></span>
+        </a>
+    <?php } ?>
 
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
         <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-            <span class="sr-only">Toggle navigation</span>
-        </a>
+        <?php if ($this->theme->layout == \webtoolsnz\AdminLte\Theme::LAYOUT_SIDEBAR_MINI) { ?>
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                <span class="sr-only">Toggle navigation</span>
+            </a>
+        <?php } ?>
+
+        <?php if ($this->theme->layout == \webtoolsnz\AdminLte\Theme::LAYOUT_TOP_NAV) { ?>
+            <a href="/" class="navbar-brand">
+                <?= Html::img('/img/logo.png', ['alt' => \Yii::$app->name]) ?>
+            </a>
+        <?php } ?>
+
         <!-- Navbar Right Menu -->
         <div class="navbar-custom-menu">
             <?= Nav::widget([
